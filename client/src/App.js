d780329eb80957fe64f0f46837710a481aa14637
Anshu Kailash <78155133+Anshuak@users.noreply.github.com>
2021-11-13 13:49:41 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import DisplayMasterData from "./components/DisplayMasterData";
 import EquipmentMasterDataForm from "./components/EquipmentMasterDataForm";
 import Navbar from "./components/Navbar";
